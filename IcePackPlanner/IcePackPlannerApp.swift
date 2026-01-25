6f445dc7efbc0912610bc5b969f56b2407d4e809
Stepan <sfermin2007@icloud.com>
2026-01-25 13:11:55 +0100

    Initial Commit

---
+++ IcePackPlanner/IcePackPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcePackPlannerApp.swift
+//  IcePackPlanner
+//
+//  Created by Stepan Yarikova on 25/1/26.
+//
+
+import SwiftUI
+
+@main
+struct IcePackPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IcePackPlannerTests/IcePackPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  IcePackPlannerTests.swift
+//  IcePackPlannerTests
+//
+//  Created by Stepan Yarikova on 25/1/26.
+//
+
+import Testing
+@testable import IcePackPlanner
+
+struct IcePackPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
